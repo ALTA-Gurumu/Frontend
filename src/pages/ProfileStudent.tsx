2b677b0790c88a0bfc842a2349a38401e8bc646d
Agung Cahya <agungcahya122@gmail.com>
2023-02-01 16:36:57 +0700

    feat: make slicing profil student page

--- src/pages/ProfileStudent.tsx
+++ src/pages/ProfileStudent.tsx
@@ -1,7 +1,86 @@
 import React from "react";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+
+import Profil from "../assets/profil.jpg";
+import Profil2 from "../assets/profil2.webp";
+
+import { FaRegEdit } from "react-icons/fa";
 
 function ProfileStudent() {
-  return <div>ProfileStudent</div>;
+  return (
+    <Layout>
+      <Navbar />
+      <div className="flex justify-center lg:py-11 py-14">
+        <div className="lg:w-[75vw] w-[90vw] h-[75vh] flex flex-col items-center rounded-2xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.4)] bg-white p-5 relative">
+          <div className="flex w-full gap-4 bg-[#FAFAFA]">
+            <p className="bg-[#EDF1FF] border-b-2 border-[#3056D3] px-8 py-3 items-center font-medium text-[18px] text-[#3056D3]">
+              Profil
+            </p>
+            <p className="px-5 py-3 items-center font-medium text-[18px] text-[#7C828F]">
+              Jadwal Saya
+            </p>
+          </div>
+
+          {/* Put this part before </body> tag */}
+          <label
+            htmlFor="my-modal-4"
+            className="flex gap-1 absolute lg:top-28 top-32 lg:right-52 right-8"
+          >
+            <FaRegEdit className="w-5 h-5 text-[#424242]" />
+            <p className="text-[#8E8E8E] lg:text-[16px] text-[14px]">
+              perbarui profile
+            </p>
+          </label>
+
+          <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+          <label htmlFor="my-modal-4" className="modal cursor-pointer">
+            <label className="modal-box relative" htmlFor="">
+              <h3 className="text-lg font-bold">
+                Congratulations random Internet user!
+              </h3>
+              <p className="py-4">
+                You've been selected for a chance to get one year of
+                subscription to use Wikipedia for free!
+              </p>
+            </label>
+          </label>
+          <div
+            className=" w-32 h-32 lg:mt-12 mt-24 border border-[#EFEFEF] rounded-full overflow-hidden mt- bg-no-repeat bg-cover"
+            style={{ backgroundImage: `URL(${Profil})` }}
+          >
+            <img src={Profil2} alt="profil.jpg" />
+          </div>
+
+          <p className="mt-2 lg:text-[36px] text-[24px] font-semibold text-[#112B3C]">
+            Diva Lesti Pujiana
+          </p>
+
+          <div className="text-[16px] flex lg:flex-row flex-col lg:w-[45vw] w-[80vw] p-2 mt-10">
+            <div className="lg:ml-10 ml-3 lg:w-8/12 w-11/12">
+              <p className="font-semibold">
+                Email
+                <span className=" pl-[16px] font-normal">
+                  : devilestipuji@gmail.com
+                </span>
+              </p>
+              <br />
+              <p className=" font-semibold">
+                Handphone
+                <span className=" pl-[6px] font-normal"> : 089566787765</span>
+              </p>
+            </div>
+            <div className="lg:ml-0 ml-3 lg:mt-0 mt-5">
+              <p className="block font-semibold">Alamat :</p>
+              <p className="font-normal">
+                Ds. Kebun Bumi Bulat, Garum, Blitar, Jawa Timur
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export default ProfileStudent;
