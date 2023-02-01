f59a65d49ac51b5718fcb9d70a33b18cd4ee90d2
pryscila dinda <pryscila.dinda06@gmail.com>
2023-02-01 16:00:26 +0700

    instalisasi axios, daisyui, tailwind, cookie,route,icon

---
+++ src/routes/App.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import axios, { AxiosHeaders } from "axios";
+import {
+  Navigate,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import Beranda from "../pages/Home";
+import LandingPage from "../pages/LandingPage";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import ProfileStudent from "../pages/ProfileStudent";
+
+function App() {
+  const [cookie, , removeCookie] = useCookies(["token"]);
+  const checkToken = cookie.token;
+
+  axios.interceptors.request.use(function (config) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${cookie.token}`;
+    return config;
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/home",
+      element: <Beranda />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    // {
+    //   path: "/detail/:id_item",
+    //   element: <DetailItems />,
+    // },
+    // {
+    //   path: "/cart",
+    //   element: checkToken ? <Cart /> : <Login />,
+    // },
+    // {
+    //   path: "/checkout",
+    //   element: checkToken ? <Checkout /> : <Login />,
+    // },
+    {
+      path: "/profile-student/:id_user",
+      element: checkToken ? <ProfileStudent /> : <Login />,
+    },
+    // {
+    //   path: "/transactions",
+    //   element: checkToken ? <Transaction /> : <Login />,
+    // },
+    // {
+    //   path: "/transactions-selling",
+    //   element: checkToken ? <TranscSell /> : <Login />,
+    // },
+    // {
+    //   path: "/profile-edit/:id_user",
+    //   element: checkToken ? <EditProfile /> : <Login />,
+    // },
+    // {
+    //   path: "/new-item",
+    //   element: checkToken ? <AddProduct /> : <Login />,
+    // },
+    // {
+    //   path: "/edit-item/:product_id",
+    //   element: checkToken ? <EditProduct /> : <Login />,
+    // },
+    // {
+    //   path: "/buying/:id_order",
+    //   element: checkToken ? <DetailTransc /> : <Login />,
+    // },
+    // {
+    //   path: "/selling/:id_order",
+    //   element: checkToken ? <DetailTranscSell /> : <Login />,
+    // },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#112B3C",
+        primary: "#EFEFEF",
+        card: "#205375",
+        component: "#F66B0E",
+      },
+    },
+    daisyui: {
+      themes: false,
+    },
+  },
+  plugins: [require("daisyui")],
+};
