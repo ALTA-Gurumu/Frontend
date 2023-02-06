856606f5f03f68b23eff27f1de1df4367755b74a
pryscila dinda <pryscila.dinda06@gmail.com>
2023-02-06 22:42:32 +0700

    fixing for deploy svg

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,7 +8,7 @@ import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import CustomBotton from "../components/CustomButton";
 import image1 from "../assets/image1.svg";
-import Selecting from "../assets/selecting.svg";
+import Profiling from "../assets/profiling.svg";
 import Request from "../assets/request.svg";
 import Calender from "../assets/calender.svg";
 import avatar2 from "../assets/avatar2.webp";
@@ -137,7 +137,7 @@ function LandingPage() {
         </div>
         <img
           className="w-[30rem]"
-          src={Selecting}
+          src={Profiling}
           alt="Pilih guru anda"
         />
       </div>
